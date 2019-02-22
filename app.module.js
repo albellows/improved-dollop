8d0d2203b55c7c07c46ba9102dd0c64a43a918fa
Allison <alibellows@gmail.com>
2019-02-21 22:16:32 -0700

    initial commit

---
+++ app.module.js
@@ -0,0 +1,8 @@
+// Defining the module
+angular.module('temperatureApp',[
+	// List app dependencies here
+	'temperatureList',
+	'currentTemperature',
+	'thresholdViolations',
+	'wovynSensorDashboard'
+]);
---
+++ temperature-list/temperature-list.component.js
@@ -0,0 +1,42 @@
+// Define the temperature component
+angular.module('temperatureList').component('temperatureList', {
+	templateUrl: 'temperature-list/temperature-list.component.html',
+	controller: ['$http', '$interval', function TemperatureListController($http, $interval) {
+		this.eci = "5vuhyTK9LZNBXcR6t5nMJA";
+		this.temperatures = [];
+
+		// These are the API URLs we need for this component
+		var _this = this;
+		var host = "http://localhost:8080/";
+		var url = host + "sky/cloud/"+this.eci+"/temperature_store/temperatures?";
+
+		// Setup an interval function
+		var liveTemperatures;
+		this.getLiveTemperatures = function(){
+				$http.get(url).then(
+					function success(response){
+						_this.temperatures = [];
+						for(i = 1; i < 10; i++){
+							if(response.data.length - i < 0) break;
+							_this.temperatures.push(response.data[response.data.length - i])
+						}
+					},
+					function error(response){
+						_this.temperatures = [];
+						_this.temperatures.push({timestamp: "Fail", temperature: "Fail"})
+				});		
+		};
+
+		liveTemperatures = $interval(this.getLiveTemperatures, 3000);
+
+		this.$onDestroy = function(){
+			if(angular.isDefined(liveTemperatures)){
+				$interval.cancel(liveTemperatures);
+				liveTemperatures = undefined;
+			}
+		};
+
+		// Initialize
+		this.getLiveTemperatures();
+	}]
+});
